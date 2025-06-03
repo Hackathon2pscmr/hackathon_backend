e27d98bb25dd3ec01bf1910a154dc01f0d4d7056
DeviLalitha11 <157714634+DeviLalitha11@users.noreply.github.com>
2025-06-03 11:06:25 +0530

    Update server.js

--- hackathon_backend/server.js
+++ hackathon_backend/server.js
@@ -24,6 +24,13 @@ mongoose.connect(process.env.MONGO_URI)
 const registrationRoute = require('./routes/registration');
 app.use('/api/registrations', registrationRoute);
 
+app.get('/', (req, res)=>{
+  res.send({
+    activeStatus:true,
+    error: false,
+  })
+});
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
